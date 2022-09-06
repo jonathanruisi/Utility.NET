90510fd0fae6acb5fd1255a2cbf541371a96e019
Jon Ruisi <jonathanruisi@gmail.com>
2022-09-06 18:23:43 -0400

    Initial attempt fully coded, but crashes with a XAML parsing error.

--- Utility.WinUI/ExtensionMethods.cs
+++ Utility.WinUI/ExtensionMethods.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.Brushes;
+using Microsoft.Graphics.Canvas.Geometry;
 using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
@@ -61,6 +63,77 @@ namespace JLR.Utility.WinUI
                 default:
                     throw new InvalidOperationException(
                         "This type of brush cannot be converted to ICanvasBrush via this method");
+            }
+        }
+        #endregion
+
+        #region Microsoft.UI.Xaml.Media.Geometry
+        public static CanvasGeometry CreateCanvasGeometry(this Geometry geometry,
+                                                          ICanvasResourceCreator resourceCreator)
+        {
+            switch (geometry)
+            {
+                case EllipseGeometry shape:
+                    return CanvasGeometry.CreateEllipse(resourceCreator,
+                        (float)shape.Center.X, (float)shape.Center.Y, (float)shape.RadiusX, (float)shape.RadiusY);
+
+                case PathGeometry shape:
+                {
+                    using var pathBuilder = new CanvasPathBuilder(resourceCreator);
+                    foreach (var figure in shape.Figures)
+                    {
+                        pathBuilder.BeginFigure((float)figure.StartPoint.X, (float)figure.StartPoint.Y);
+
+                        foreach (var segment in figure.Segments)
+                        {
+                            switch (segment)
+                            {
+                                case ArcSegment seg:
+                                    pathBuilder.AddArc(new Vector2((float)seg.Point.X, (float)seg.Point.Y),
+                                                       (float)seg.Size.Width, (float)seg.Size.Height,
+                                                       (float)seg.RotationAngle,
+                                                       seg.SweepDirection == SweepDirection.Clockwise ? CanvasSweepDirection.Clockwise : CanvasSweepDirection.CounterClockwise,
+                                                       seg.IsLargeArc ? CanvasArcSize.Large : CanvasArcSize.Small);
+                                    break;
+
+                                case BezierSegment seg:
+                                    pathBuilder.AddCubicBezier(new Vector2((float)seg.Point1.X, (float)seg.Point1.Y),
+                                                               new Vector2((float)seg.Point2.X, (float)seg.Point2.Y),
+                                                               new Vector2((float)seg.Point3.X, (float)seg.Point3.Y));
+                                    break;
+
+                                case LineSegment seg:
+                                    pathBuilder.AddLine((float)seg.Point.X, (float)seg.Point.Y);
+                                    break;
+
+                                case PolyLineSegment seg:
+                                    foreach (var p in seg.Points)
+                                        pathBuilder.AddLine(new Vector2((float)p.X, (float)p.Y));
+                                    break;
+
+                                case QuadraticBezierSegment seg:
+                                    pathBuilder.AddQuadraticBezier(new Vector2((float)seg.Point1.X, (float)seg.Point1.Y),
+                                                                   new Vector2((float)seg.Point2.X, (float)seg.Point2.Y));
+                                    break;
+
+                                default:
+                                    throw new InvalidOperationException(
+                                        "This type of figure cannot be added to a CanvasGeometry using this method");
+                            }
+                        }
+
+                        pathBuilder.EndFigure(figure.IsClosed ? CanvasFigureLoop.Closed : CanvasFigureLoop.Open);
+                    }
+
+                    return CanvasGeometry.CreatePath(pathBuilder);
+                }
+
+                case RectangleGeometry shape:
+                    return CanvasGeometry.CreateRectangle(resourceCreator, shape.Rect);
+
+                default:
+                    throw new InvalidOperationException(
+                        "This type of geometry cannot be converted to CanvasGeometry via this method");
             }
         }
         #endregion
