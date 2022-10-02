ea734cd56cd302d2fd6729426e582094ec41756c
Jon Ruisi <jonathanruisi@gmail.com>
2022-10-02 03:55:52 -0400

    Added extension method to get the file extension of a StorageFile

--- Utility.WinUI/ExtensionMethods.cs
+++ Utility.WinUI/ExtensionMethods.cs
@@ -9,6 +9,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Media;
 
 using Windows.Foundation;
+using Windows.Storage;
 
 using WinRT.Interop;
 
@@ -69,6 +70,13 @@ namespace JLR.Utility.WinUI
         public static Point GetCenterPoint(this Rect rect)
         {
             return new Point(rect.X + (rect.Width / 2), rect.Y + (rect.Height / 2));
+        }
+        #endregion
+
+        #region Windows.Storage.StorageFile
+        public static string GetFileExtension(this StorageFile file)
+        {
+            return file.Name.Split('.', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Last().ToLower();
         }
         #endregion
     }
