ed744e639b0a9f8ad23ceb371250ed4c2844ef79
Jon Ruisi <jonathanruisi@gmail.com>
2022-10-14 01:36:12 -0400

    DebugLogControl is now a UserControl

---
+++ Utility.WinUI/Controls/DebugLogControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace JLR.Utility.WinUI.Controls
+{
+    public sealed partial class DebugLogControl : UserControl
+    {
+        #region Fields
+
+        #endregion
+
+        #region Constructor
+        public DebugLogControl()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+
+        #region Dependency Property Callbacks
+
+        #endregion
+
+        #region Event Handlers
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
